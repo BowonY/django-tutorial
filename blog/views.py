decee2f8655c830058725f740743034a7221e8a7
bowon <bwyang0902@gmail.com>
2016-06-25 21:32:06 +0900

    first home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'blog/home.html', {})
+
+def post_list(request):
+    # render (put together) my template blog/post_list.html
+    # returns a HttpResponse
+    return render(request, 'blog/post_list.html', {})
